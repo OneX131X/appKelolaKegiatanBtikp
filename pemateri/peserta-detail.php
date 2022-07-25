8349b38bdaaca1f839ccd386f17aade837ba9c98
OneX131X <kurniawanrasyidi1321@gmail.com>
2022-07-26 01:37:44 +0800

    Fixed in peserta detail on pemateri

--- pemateri/peserta-detail.php
+++ pemateri/peserta-detail.php
@@ -16,38 +16,18 @@ include '../koneksi.php';
 
 $id = $_GET["id"];
 $query_peserta = "SELECT 
-                    peserta.*, 
-                    peserta_aktifitas.*,
+                    peserta_aktifitas.*, 
+                    peserta.nama_peserta, 
                     kegiatan.nama_kegiatan 
                     FROM 
                     peserta_aktifitas, peserta, kegiatan 
                     WHERE 
-                    peserta.id = peserta_aktifitas.id_peserta AND 
-                    kegiatan.id = peserta_aktifitas.id_kegiatan AND 
-                    peserta_aktifitas.id_peserta = $id";
+                    peserta.id = peserta_aktifitas.id_peserta AND
+                    kegiatan.id = peserta_aktifitas.id_kegiatan AND
+                    peserta_aktifitas.id_peserta = '$id'";
 $result_peserta = mysqli_query($conn, $query_peserta);
 $row_peserta = mysqli_fetch_assoc($result_peserta);
 
-if (isset($_POST["submit"])) {
-
-    $penilaian = htmlspecialchars($_POST["penilaian"]);
-
-    $query = "UPDATE peserta_aktifitas SET 
-                penilaian = '$penilaian' 
-                WHERE id_peserta = $id ";
-    $edit = mysqli_query($conn, $query);
-
-    if ($edit) {
-        echo "<script type='text/javascript'>
-                alert('Data berhasil disimpan...!');
-                document.location.href = 'peserta.php';
-            </script>";
-        } else {
-        echo "<script type='text/javascript'>
-                alert('Data GAGAL disimpan...!');
-                </script>";        
-        }
-}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -55,10 +35,61 @@ if (isset($_POST["submit"])) {
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Data Peserta | APPKelolaKegiatanBTIKP</title>
+    <title>Detail Peserta | APPKelolaKegiatanBTIKP</title>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
     <link rel="stylesheet" href="../plugins/fontawesome-free/css/all.min.css">
     <link rel="stylesheet" href="../dist/css/adminlte.min.css">
+    <style>
+        .sertifikat{
+            display: flex;
+            float: right;
+            margin-top: 20px;
+        }
+        .btn-sertifikat{
+            background-color: #ffcc00;
+            border: none;
+            color: black;
+            text-align: center;
+            font-size: 16px;
+            cursor: pointer;
+            float: right;
+        }
+        .btn-sertifikat:hover{
+            background-color: #e6b800;
+            color: black;
+        }
+        .detail {
+            font-family: sans-serif;
+            color: #232323;
+            border-collapse: collapse;
+            width: 100%;
+            height: 200px;
+            line-height: 30px;
+            /* margin-top: 40px; */
+        }
+        /* .detail td, th, tr{
+            border: 1px solid black;
+        } */
+        .detail td, th{
+            padding: 10px 15px 10px 15px;
+        }
+        .detail th:nth-child(1){
+            width: 100%;
+            height: 100%;
+            text-align: center;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+        .detail td:nth-child(3){
+            width: 100%;
+            height: 100%;
+            text-align: center;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+    </style>
 </head>
 
 <body class="hold-transition sidebar-mini">
@@ -75,13 +106,15 @@ if (isset($_POST["submit"])) {
                 <div class="container-fluid">
                     <div class="row mb-2">
                         <div class="col-sm-6">
-                            <h1>Data Peserta</h1>
+                            <h1>Detail Peserta</h1>
                         </div>
                         <div class="col-sm-6">
                             <ol class="breadcrumb float-sm-right">
                                 <li class="breadcrumb-item"><a href="index.php">Home</a></li>
-                                <li class="breadcrumb-item"><a href="peserta.php">Peserta</a></li>
-                                <li class="breadcrumb-item active">Edit Peserta</li>
+                                <li class="breadcrumb-item active">Penilaian</li>
+                                <li class="breadcrumb-item"><a href="kegiatan.php">List Kegiatan</a></li>
+                                <li class="breadcrumb-item"><a href="peserta.php?id=<?= $row_peserta["id_kegiatan"]; ?>">List Peserta</a></li>
+                                <li class="breadcrumb-item active">Detail Peserta</li>
                             </ol>
                         </div>
                     </div>
@@ -94,45 +127,27 @@ if (isset($_POST["submit"])) {
                     <div class="row">
                         <div class="col-md-12">
                             <!-- general form elements -->
-                            <div class="card card-primary">
+                            <div class="card card-info">
                                 <div class="card-header">
-                                    <h3 class="card-title">Tambah Data</h3>
+                                    <h3 class="card-title">Detail Data Peserta</h3>
                                 </div>
                                 <!-- /.card-header -->
-                                <!-- form start -->
-                                <form action="" method="post">
-                                    <div class="card-body">
-                                        <div class="form-group">
-                                            <label for="nama_peserta">Nama Peserta :</label>
-                                            <input type="text" class="form-control" id="nama_peserta" name="nama_peserta" value="<?php echo $row_peserta["nama_peserta"]; ?>" placeholder="Nama Peserta" readonly>
-                                        </div>
-                                        <div class="form-group">
-                                            <label for="noTelp">Nomor Telepon :</label>
-                                            <input type="text" class="form-control" id="noTelp" name="noTelp" maxlength="13" value="<?php echo $row_peserta["noTelp"]; ?>" placeholder="Nomor Telepon" readonly>
-                                        </div>
-                                        <div class="form-group">
-                                            <label for="email">Email :</label>
-                                            <input type="text" class="form-control" id="email" name="email" value="<?php echo $row_peserta["email"]; ?>" placeholder="Email" readonly>
-                                        </div>
-                                        <div class="form-group">
-                                            <label for="nama_kegiatan">Kegiatan :</label>
-                                            <input type="text" class="form-control" id="nama_kegiatan" name="nama_kegiatan" value="<?php echo $row_peserta["nama_kegiatan"]; ?>" placeholder="Nama Kegiatan" readonly>
-                                        </div>
-                                        <div class="form-group">
-                                            <label for="penilaian">Penilain :</label>
-                                            <input type="text" class="form-control" id="penilaian" name="penilaian" value="<?php if($row_peserta["penilaian"] == "") { 
-                                                echo "Belum Dinilai";
-                                            } else {
-                                                echo $row_peserta["penilaian"]; } ?>" placeholder="Penilaian Hasil Bimtek" readonly>
-                                        </div>
+                                    <div class="card-body table-responsive pad">
+                                        <table>
+                                            <tr>
+                                                <td for="nama_peserta" style="width: 23%;">Nama Peserta</td>
+                                                <td style="width: 2%;">:</td>
+                                                <td><?= $row_peserta["nama_peserta"]; ?></td>
+                                            </tr>
+                                            <tr>
+                                                <td for="nama_kegiatan">Kegiatan</td>
+                                                <td>:</td>
+                                                <td><?= $row_peserta["nama_kegiatan"]; ?></td>
+                                            </tr>
+                                        </table>
+                                        <!-- /.card-body -->
                                     </div>
-                                    <!-- /.card-body -->
-
-                                    <div class="card-footer">
-                                        <button type="submit" class="btn btn-primary mr-1" name="submit">Simpan</button>
-                                        <a href="peserta.php" class="btn btn-secondary">Cancel</a>
-                                    </div>
-                                </form>
+                                <!-- /.card-body -->
                             </div>
                         </div>
                         <!-- /.col -->
@@ -142,6 +157,131 @@ if (isset($_POST["submit"])) {
                 <!-- /.container-fluid -->
             </section>
             <!-- /.content -->
+            <section class="content">
+                    <div class="container-fluid">
+                        <div class="row">
+                            <div class="col-12">
+                                <div class="card">
+                                    <div class="card ">
+                                        <div class="card-header bg-lime">
+                                            <h3 class="card-title">Detail Kegiatan</h3>
+                                        </div>
+                                        <!-- /.card-header -->
+                                        <div class="card-body">
+                                            <div class="card-header bg-orange">
+                                                <h3 class="card-title"><?php echo $row_peserta["nama_kegiatan"]; ?></h3>
+                                            </div>
+                                            <table id="example1" class="table table-bordered table-striped detail">
+                                                <thead>
+                                                    <tr>
+                                                        <th>Hari</th>
+                                                        <th>Aktifitas</th>
+                                                        <th style="text-align: center;">Absensi</th>
+                                                    </tr>
+                                                </thead>
+                                                <tbody>
+                                                    <?php $no = 1;
+                                                    $result = mysqli_query($conn, "SELECT 
+                                                                detail_kegiatan.*, 
+                                                                peserta_aktifitas.*, 
+                                                                peserta.nama_peserta, 
+                                                                kegiatan.nama_kegiatan 
+                                                                FROM 
+                                                                detail_kegiatan, peserta_aktifitas, peserta, kegiatan 
+                                                                WHERE 
+                                                                peserta.id = peserta_aktifitas.id_peserta AND 
+                                                                kegiatan.id = peserta_aktifitas.id_kegiatan AND 
+                                                                peserta_aktifitas.id_kegiatan = detail_kegiatan.id_kegiatan AND 
+                                                                peserta_aktifitas.id_peserta = $id");
+                                                    while ($row = mysqli_fetch_assoc($result)) { ?>
+                                                        <tr>
+                                                            <th>Hari I</th>
+                                                            <td>
+                                                                <?php 
+                                                                $arr = explode(",", $row["hari_satu"]); 
+                                                                $tes = count($arr);
+                                                            
+                                                                foreach ($arr as $i) {
+                                                                    echo '<b>-> </b>' . $i . '<br>';
+                                                                }   
+                                                                ?>
+                                                            </td>
+                                                            <td><?php if ($row["absen1"] == "hadir") {
+                                                                echo "Hadir";
+                                                            } else { 
+                                                                echo "Tidak Hadir";
+                                                            }?></td>
+                                                        </tr>
+                                                        <tr>
+                                                            <th>Hari II</th>
+                                                            <td>
+                                                                <?php 
+                                                                $arr = explode(",", $row["hari_dua"]); 
+                                                                foreach ($arr as $i) {
+                                                                    echo '<b>-> </b>' . $i . '<br>';
+                                                                }   
+                                                                ?>
+                                                            </td>
+                                                            <td><?php if ($row["absen2"] == "hadir") {
+                                                                echo "Hadir";
+                                                            } else { 
+                                                                echo "Tidak Hadir";
+                                                            }?></td>
+                                                        </tr>
+                                                        <tr>
+                                                            <th>Hari III</th>
+                                                            <td>
+                                                                <?php 
+                                                                $arr = explode(",", $row["hari_tiga"]); 
+                                                                foreach ($arr as $i) {
+                                                                    echo '<b>-> </b>' . $i . '<br>';
+                                                                }   
+                                                                ?>
+                                                            </td>
+                                                            <td><?php if ($row["absen3"] == "hadir") {
+                                                                echo "Hadir";
+                                                            } else { 
+                                                                echo "Tidak Hadir";
+                                                            }?></td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td align="right" class="mr-3" colspan="2">Nilai</td>
+                                                            <td align="center">
+                                                                <?php 
+                                                                $nilai = $row["penilaian"]; 
+                                                                switch ($nilai) {
+                                                                    case "sangat baik":
+                                                                        echo "Sangat Baik";
+                                                                        break;
+                                                                    case "cukup baik":
+                                                                        echo "Cukup Baik";
+                                                                        break;
+                                                                    default:
+                                                                        echo "Baik";
+                                                                        break;
+                                                                }
+                                                                ?>
+                                                            </td>
+                                                        </tr>
+                                                    <?php $no++;
+                                                    } ?>
+                                                </tbody>
+                                            </table>
+                                            <div class="sertifikat">
+                                                <a href="../cetak-peserta-sertifikat2.php?id=<?= $row_peserta['id']; ?>" class="btn btn-sertifikat" target="_blank"><i class="fa fa-download mr-1"></i> Cetak Sertifikat</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <!-- /.card-body -->
+                                </div>
+                                <!-- /.card -->
+                            </div>
+                            <!-- /.col -->
+                        </div>
+                        <!-- /.row -->
+                    </div>
+                    <!-- /.container-fluid -->
+                </section>
         </div>
         <!-- /.content-wrapper -->
 
